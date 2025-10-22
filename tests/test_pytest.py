322da6d5db9d5d97ef643bd2ae56a946795a9647
BioWarricK <otrubenikov.nik@mail.ru>
2025-10-22 19:04:11 +0700

    Интеграция Pytest и Playwright

---
+++ tests/test_pytest.py
@@ -0,0 +1,13 @@
+def test_first_try():
+    print('Hello, Pytest!')
+
+#
+# def test_greeting():
+#     greeting = "Hello!"
+#     assert greeting == "Hi!", "Строки не совпадают!"
+#
+# def test_assert_positive_case():
+#     assert (2 + 2) == 4
+#
+# def test_assert_negative_case():
+#     assert (2 + 2) == 5
