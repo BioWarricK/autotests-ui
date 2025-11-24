508a8bb33ab7206f9faa0e93aa5ccca47e1616c1
BioWarricK <otrubenikov.nik@mail.ru>
2025-11-24 19:56:49 +0700

    Практикуемся в реализации PageObject

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -8,19 +8,24 @@ class DashboardPage(BasePage):
 
 
         self.dashboard_title_text = page.get_by_test_id('dashboard-toolbar-title-text')
-        self.students_widget_text = page.get_by_test_id('students-widget-title-text')
-        self.activities_widget_text = page.get_by_test_id('activities-widget-title-text')
-        self.courses_widget_text = page.get_by_test_id('courses-widget-title-text')
-        self.scores_widget_text =  page.get_by_test_id('scores-widget-title-text')
-        self.dashboard_button = page.get_by_test_id('dashboard-drawer-list-item-button')
-        self.courses_button = page.get_by_test_id('courses-drawer-list-item-button')
-        self.logout_button = page.get_by_test_id('logout-drawer-list-item-button')
-        self.navbar_title_text = page.get_by_test_id('navigation-navbar-app-title-text')
-        self.navbar_welcome_text = page.get_by_test_id('navigation-navbar-welcome-title-text')
+
+        self.students_widget_title = page.get_by_test_id('students-widget-title-text')
         self.students_chart = page.get_by_test_id('students-bar-chart')
+
+        self.activities_widget_title = page.get_by_test_id('activities-widget-title-text')
         self.activities_chart = page.get_by_test_id('activities-line-chart')
+
+        self.courses_widget_title = page.get_by_test_id('courses-widget-title-text')
         self.courses_chart = page.get_by_test_id('courses-pie-chart')
+
+        self.scores_widget_title =  page.get_by_test_id('scores-widget-title-text')
         self.scores_chart = page.get_by_test_id('scores-scatter-chart')
+
+        # self.dashboard_button = page.get_by_test_id('dashboard-drawer-list-item-button')
+        # self.courses_button = page.get_by_test_id('courses-drawer-list-item-button')
+        # self.logout_button = page.get_by_test_id('logout-drawer-list-item-button')
+        # self.navbar_title_text = page.get_by_test_id('navigation-navbar-app-title-text')
+        # self.navbar_welcome_text = page.get_by_test_id('navigation-navbar-welcome-title-text')
 
 
     def check_dashboard_title_text(self):
@@ -29,3 +34,43 @@ class DashboardPage(BasePage):
         """
         expect(self.dashboard_title_text).to_be_visible()
         expect(self.dashboard_title_text).to_have_text('Dashboard')
+
+
+    def check_visible_students_chart(self):
+        """
+         Проверка видимости виджета Students и его заголовка
+        :return:
+        """
+        expect(self.students_widget_title).to_be_visible()
+        expect(self.students_widget_title).to_have_text('Students')
+        expect(self.students_chart).to_be_visible()
+
+
+    def check_visible_activities_chart(self):
+        """
+         Проверка видимости виджета Activities и его заголовка
+        :return:
+        """
+        expect(self.activities_widget_title).to_be_visible()
+        expect(self.activities_widget_title).to_have_text('Activities')
+        expect(self.activities_chart).to_be_visible()
+
+
+    def check_visible_courses_chart(self):
+        """
+         Проверка видимости виджета Courses и его заголовка
+        :return:
+        """
+        expect(self.courses_widget_title).to_be_visible()
+        expect(self.activities_widget_title).to_have_text('Courses')
+        expect(self.activities_chart).to_be_visible()
+
+
+    def check_visible_scores_chart(self):
+        """
+         Проверка видимости виджета Scores и его заголовка
+        :return:
+        """
+        expect(self.scores_widget_title).to_be_visible()
+        expect(self.scores_widget_title).to_have_text('Scores')
+        expect(self.scores_chart).to_be_visible()
