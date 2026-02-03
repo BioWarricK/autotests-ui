bdf7f6411b6380f43defa6e12f814b607effcfe9
BioWarricK <otrubenikov.nik@mail.ru>
2026-02-03 19:24:05 +0700

    Запуск автотестов на разных браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -7,14 +7,18 @@ from tools.playwright.pages import initialize_playwright_page
 from config import settings
 
 
-@pytest.fixture()
+@pytest.fixture(params=settings.browsers)
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param
+    )
 
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=settings.headless)  # Используем settings.headless
+    browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
@@ -27,14 +31,15 @@ def initialize_browser_state(playwright: Playwright):
     )
     registration_page.click_registration_button()
 
-    context.storage_state(path=settings.browser_state_file)  # Используем settings.browser_state_file
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state=settings.browser_state_file  # Используем settings.browser_state_file
+        storage_state=settings.browser_state_file,
+        browser_type=request.param
     )
